605ff9aedcf52c8ee0c68fc31a35fee01d7bc3f0
Jani Arvonen <jani.arvonen@gmail.com>
2016-02-27 19:21:38 +0200

    initial commit

---
+++ run_web_dev.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker/docker-compose.yml -p busa run --rm --service-ports --name busa-web-dev web
---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const pkg = require('./package.json');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/main.css')
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  entry: {
+    app: PATHS.app,
+    style: PATHS.style
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'Busa App',
+      appMountId: 'app',
+      inject: false
+    })
+  ]
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // display only errors to reduce the amount of output
+      stats: 'errors-only',
+
+      // parse host and port from env so this is easy
+      // to customize
+      host: "0.0.0.0",
+      port: process.env.PORT
+    },
+    module: {
+      loaders: [
+        // Define development specific CSS setup
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build' || TARGET === 'stats') {
+  module.exports = merge(common, {
+    entry: {
+      vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        // Extract CSS during build
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new CleanPlugin([PATHS.build]),
+      new ExtractTextPlugin('[name].[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin({names: ['vendor', 'manifest']}),
+      new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
+      new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+    ]
+  });
+}
