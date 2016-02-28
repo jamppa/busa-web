652254119635078da0c26a420fc7aac1fd3bea77
Jani Arvonen <jani.arvonen@gmail.com>
2016-02-28 14:57:07 +0200

    chore(package): introduce material-ui and react-grid-layout

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/app.jsx';
+
+injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('app'));
