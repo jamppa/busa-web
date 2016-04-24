573d4314a587374f87a8e8d12403610513f7709b
Jani Arvonen <jani.arvonen@gmail.com>
2016-04-24 20:04:54 +0300

    test(connection-card): should render first element as material-ui Paper

---
+++ test/components/connection-card-test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import ConnectionCard from '../../app/components/connection-card';
+
+describe('ConnectionCard Component', () => {
+
+  it('should render connection places', () => {
+    
+  });
+});
