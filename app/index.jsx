9ac514de85cb9c0c34819c4a342c7bd55f62a6ea
Jani Arvonen <jani.arvonen@gmail.com>
2016-05-05 19:47:13 +0300

    chore(app): introduce redux with initial store and reducers setup

--- app/index.jsx
+++ app/index.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { combineReducers, createStore } from 'redux';
+
 import App from './app.jsx';
 import DeparturingNext from './views/departuring-next.jsx';
+import * as reducers from './reducers';
 
 import './styles/main.css';
 
 injectTapEventPlugin();
+
+const busaReducers = combineReducers(reducers);
+const store = createStore(busaReducers);
 
 const BusaRouter = React.createClass({
 
