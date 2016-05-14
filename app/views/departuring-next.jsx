7a0d82751899c87597cd3c532c045008e027623a
Jani Arvonen <jani.arvonen@gmail.com>
2016-05-14 21:54:47 +0300

    feat(departuring-next): fetch connections departuring next on componentDidMount

--- app/views/departuring-next.jsx
+++ app/views/departuring-next.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions/connections-actions';
+
 import ConnectionCard from '../components/connection-card.jsx';
 
 export const DeparturingNext = React.createClass({
 
+  propTypes: {
+    fetchConnectionsDeparturingNext: React.PropTypes.func.isRequired,
+    connections: React.PropTypes.array.isRequired
+  },
+
+  getDefaultProps() {
+    return {
+      fetchConnectionsDeparturingNext: () => {},
+      connections: []
+    };
+  },
+
+  componentDidMount() {
+    this.props.fetchConnectionsDeparturingNext();
+  },
+
   render() {
+
+    const connectionCards = this.props.connections.map((conn, index) => {
+      return (<ConnectionCard key={index} connection={conn} />);
+    });
+
     return(
       <div className="pure-g">
         <div className="pure-u-1 pure-u-md-1-4">
-          {this.props.connections.map(c => {
-            return (
-              <ConnectionCard connection={c} />
-            );
-          })}
+          {connectionCards}
         </div>
       </div>
     );
@@ -25,4 +44,4 @@ function mapStateToProps(state) {
   };
 }
 
-export const DeparturingNextConnected = connect(mapStateToProps)(DeparturingNext);
+export const DeparturingNextConnected = connect(mapStateToProps, actions)(DeparturingNext);
