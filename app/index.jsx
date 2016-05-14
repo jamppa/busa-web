7da35074ab289aa0d5d3fd8ca53637ad3d0c325a
Jani Arvonen <jani.arvonen@gmail.com>
2016-05-14 21:49:37 +0300

    chore(app/index): apply redux-thunk middleware

--- app/index.jsx
+++ app/index.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { combineReducers, createStore } from 'redux';
+
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
 
 import App from './app.jsx';
 import { DeparturingNextConnected } from './views/departuring-next.jsx';
@@ -14,13 +16,15 @@ import './styles/main.css';
 injectTapEventPlugin();
 
 const busaReducers = combineReducers(reducers);
-const store = createStore(busaReducers);
-store.dispatch({
+const store = createStore(busaReducers, applyMiddleware(thunkMiddleware));
+
+/*store.dispatch({
   type: 'RECEIVE_CONNECTIONS_DEPARTURING_NEXT',
   payload: {
     connections: [{from: {name: 'Nummela', time: '2016-04-24T22:30:00+03:00'}, to: {name: 'Helsinki', time: '2016-04-24T23:20:00+03:00'}}]
   }
 });
+*/
 
 const BusaRouter = React.createClass({
 
