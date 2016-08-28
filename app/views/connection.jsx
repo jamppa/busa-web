923156ddd305be15fa25403c1ca47699c3f4a98d
Jani Arvonen <jani.arvonen@gmail.com>
2016-08-28 20:33:32 +0300

    feat(views/connection): show loader while loading connections from server

--- app/views/connection.jsx
+++ app/views/connection.jsx
@@ -5,6 +5,7 @@ import * as actions from '../actions/connections-actions';
 import FloatingActionButton from 'material-ui/lib/floating-action-button';
 import RefreshIcon from 'material-ui/lib/svg-icons/action/cached';
 import ConnectionCard from '../components/connection-card.jsx';
+import LoaderSpinner from '../components/loader-spinner.jsx';
 
 const Connection = React.createClass({
 
@@ -24,8 +25,7 @@ const Connection = React.createClass({
     this.props.fetchConnectionsByPlaces(this.props.params.from, this.props.params.to);
   },
 
-  render() {
-
+  renderConnectionCards() {
     const connectionCards = this.props.connections.map((conn, index) => {
       return (
         <div key={index} className="pure-u-md-1 pure-u-lg-1-4">
@@ -33,6 +33,26 @@ const Connection = React.createClass({
         </div>
         );
     });
+    return connectionCards;
+  },
+
+  renderSpinner() {
+    return (
+      <div className="pure-u-1" style={{textAlign: 'center'}}>
+        <LoaderSpinner />
+      </div>
+    );
+  },
+
+  render() {
+
+    let content;
+    if(this.props.isLoading) {
+      content = this.renderSpinner();
+    }
+    else {
+      content = this.renderConnectionCards();
+    }
 
 
     return (
@@ -42,7 +62,7 @@ const Connection = React.createClass({
             <h1 className="uppercase">seuraavat lähdöt / {this.props.params.from} - {this.props.params.to}</h1>
           </div>
         </div>
-        { connectionCards }
+        { content }
       </div>
     );
   }
@@ -50,7 +70,8 @@ const Connection = React.createClass({
 
 const mapStateToProps = (state) => {
   return {
-    connections: state.connections.get('byPlaces').toJS()
+    connections: state.connections.get('byPlaces').toJS(),
+    isLoading: state.connections.get('isLoadingByPlaces')
   };
 }
 
